67351d06895306bbb72fc2d8df4e4d88504f25d7
mazen-ahmed <mazenreal22@gmail.com>
2024-04-20 23:48:52 +0200

    sitemap handling

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,10 +1,7 @@
-import {MetadataRoute} from 'next';
  
-// Can be imported from shared config
 const defaultLocale = 'en' ;
 const locales = ['en', 'ar','ru'] ;
  
-// Adapt this as necessary
 const pathnames = [
   '/',
   "/contact",
@@ -16,20 +13,26 @@ const pathnames = [
   "/services/Ecommerce",
 ];
 
-const host = "https://trust-agency.netlify.app"
- 
+const host = "https://trust-agency.netlify.app";
+
 export default function sitemap() {
-  function getUrl(pathname, locale) {
-    return `${host}/${locale}${pathname === '/' ? '' : pathname}`;
-  }
- 
-  return pathnames.map((pathname) => ({
-    url: getUrl(pathname, defaultLocale),
-    lastModified: new Date(),
-    alternates: {
-      languages: Object.fromEntries(
-        locales.map((locale) => [locale, getUrl(pathname, locale)])
-      )
-    }
-  }));
+  return pathnames.map((pathname) => {
+    return locales.map((locale) => ({
+      url: getUrl(pathname, locale),
+      lastModified: new Date(),
+      alternates: {
+        languages: locales.map((otherLocale) => ({
+          'xhtml:link': {
+            '@rel': 'alternate',
+            '@hreflang': otherLocale,
+            '@href': getUrl(pathname, otherLocale),
+          },
+        })),
+      },
+    }));
+  }).flat();
+}
+
+function getUrl(pathname, locale) {
+  return `${host}/${locale}${pathname === '/' ? '' : pathname}`;
 }
